cb41f654f154b1f684b00368cf0205a0c1db4df8
NhiiAdjetey <48660443+NhiiAdjetey@users.noreply.github.com>
2025-02-05 00:38:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+newSceneKey: {
+    text: `Your rhyming story here`,
+    img: "Your image URL here",
+    choices: [
+      { text: "Choice 1 text", nextScene: "nextSceneKey1" },
+      { text: "Choice 2 text", nextScene: "nextSceneKey2" },
+      { text: "Choice 3 text", nextScene: "nextSceneKey3" }
+    ]
+  },
+  
